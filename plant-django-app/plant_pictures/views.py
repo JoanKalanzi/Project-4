ee2eef2ffa5e05a0f3e5443820c30b5f82235c5b
joankalanzi <jaon.kalanzi1@gmail.comgit config --global user.name joan>
2021-07-21 18:17:56 +0100

    added urls for plant_pictures

--- plant-django-app/plant_pictures/views.py
+++ plant-django-app/plant_pictures/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Picture
+from .serializers import PictureSerializer
+
+
+class PictureListView(APIView):
+
+  def get(self, _request):
+    pictures = Picture.objects.all()
+    serialized_pictures = PictureSerializer(pictures, many=True)
+    return Response(serialized_pictures.data, status=status.HTTP_200_OK)
+
+
