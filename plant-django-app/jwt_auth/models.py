36da9370665ed9ea368b9dd879e2d87a1d48944d
joankalanzi <jaon.kalanzi1@gmail.comgit config --global user.name joan>
2021-07-22 13:27:18 +0100

    added users and seeds file in the jwt_auth app

--- plant-django-app/jwt_auth/models.py
+++ plant-django-app/jwt_auth/models.py
@@ -5,5 +5,4 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
   email = models.CharField(max_length=50, unique=True)
   first_name = models.CharField(max_length=50)
-  last_name = models.CharField(max_length=50)
   profile_image = models.CharField(max_length=300)
--- plant-django-app/plant_pictures/views.py
+++ plant-django-app/plant_pictures/views.py
@@ -2,12 +2,14 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.exceptions import NotFound
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import Picture
 from .serializers import PictureSerializer
 
 
 class PictureListView(APIView):
+  permission_classes = (IsAuthenticatedOrReadOnly, )
 
   def get(self, _request):
     pictures = Picture.objects.all()
@@ -26,6 +28,7 @@ class PictureListView(APIView):
 
 
 class PictureDetailView(APIView):
+  permission_classes = (IsAuthenticatedOrReadOnly, )
 
   def get_picture(self, pk):
     try:
