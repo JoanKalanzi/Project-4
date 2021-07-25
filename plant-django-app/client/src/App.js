7adc46309fd70598724cb201cde9d087846048b6
joankalanzi <jaon.kalanzi1@gmail.comgit config --global user.name joan>
2021-07-25 20:35:13 +0100

    added css to login page

--- plant-django-app/client/src/App.js
+++ plant-django-app/client/src/App.js
@@ -1,36 +1,22 @@
 // import React, { useEffect } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Home from './components/Home'
+// import Home from './components/Home'
 import Register from './components/authentication/Register'
 import Login from './components/authentication/Login'
-import PlantPictures from './components/PlantPictures'
+// import PlantPictures from './components/PlantPictures'
 
 
 const App = () => {
 
-  // useEffect(() => {
-
-  //   const getData = async () => {
-  //     //  install at axios yarn add axios
-  //     const res = await fetch('/api/plant_pictures')
-  //     const data = await res.json()
-  //     console.log(data)
-  //   }
-  //   getData()
-  // })
-
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="plant_pictures">
-          <PlantPictures />
+        <Route path = "/register">
+          <Register />
         </Route>
         <Route path="/">
-          <Home />
-          <Register />
           <Login />
-        </Route>
-
+        </Route>  
       </Switch>
     </BrowserRouter>
   )
--- plant-django-app/client/src/components/authentication/Login.js
+++ plant-django-app/client/src/components/authentication/Login.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 import axios from 'axios'
+import plantmom from '../../assets/plantmom.jpeg'
+
+
 
 const Login = () => {
   const history = useHistory()
@@ -24,7 +27,7 @@ const Login = () => {
     try {
       const { data } = await axios.post('api/auth/login/', formdata)
       setTokenToLocalStorage(data.token)
-      history.push('/plant_pictures')
+      history.push('/plant_pictures/')
     } catch (err) {
       console.log(err)
     }
@@ -33,41 +36,54 @@ const Login = () => {
   return (
     <section className="section">
       <div className="container">
-        <div className="columns">
-          <form  onSubmit = {handleSubmit} className="column is-half is-offset-one-quarter">
-            <div className="field">
-              <label className="label">Email</label>
-              <div className="control">
-                <input
-                  onChange={handleChange}
-                  className="input"
-                  placeholder="Email"
-                  name="email"
-                  value={formdata.email}
-                />
+        <div className="columns is-vcentered">
+          <div className="column is-half">
+            <img src={plantmom} alt="plantmom"/>
+          </div>
+          <div className="column is-vcentered ">
+            <div className="has-text-centered">
+              <h1 className ="is-size-1 title mb-6">PLANTM🌻M</h1>
+            </div>
+            <form onSubmit={handleSubmit} className="column is-half is-offset-one-quarter">
+              <div className="field">
+                <label className="label mb-4">Email</label>
+                <div className="control">
+                  <input
+                    onChange={handleChange}
+                    className="input"
+                    placeholder="Email"
+                    name="email"
+                    value={formdata.email}
+                  />
+                </div>
               </div>
-            </div>
-            <div className="field">
-              <label className="label">Password</label>
-              <div className="control">
-                <input
-                  type="password"
-                  onChange={handleChange}
-                  className="input"
-                  placeholder="Password"
-                  name="password"
-                  value={formdata.password}
-                />
+              <div className="field">
+                <label className="label mb-4">Password</label>
+                <div className="control">
+                  <input
+                    type="password"
+                    onChange={handleChange}
+                    className="input"
+                    placeholder="Password"
+                    name="password"
+                    value={formdata.password}
+                  />
+                </div>
               </div>
-            </div>
-            <div className="field">
-              <button type="submit" className="button is-fullwidth is-warning">Login</button>
-            </div>
-          </form>
+              <div className="field">
+                <button type="submit" className="button is-fullwidth is-warning mb-4">Login</button>
+              </div>
+              <hr/>
+              <div className="field mb-4"></div>
+              <div className="field mb-4"><p className="mb-4"> Do not have an account?<Link to="/register">SignUp</Link></p></div>
+            </form>
+          </div>
         </div>
-        <div>{ <p> Do not have an account? <Link to ="/register">SignUp</Link></p> }</div>
+
       </div>
     </section>
+
+
 
   )
 
